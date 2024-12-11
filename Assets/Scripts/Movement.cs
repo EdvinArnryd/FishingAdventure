0a5b846730f58a2adc901a3a8036ae49271e2cd1
unknown <edvin.arn@hotmail.com>
2024-12-11 23:19:00 +0100

    Added assets

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector2(horizontal, vertical);
+
+        transform.position += direction * speed * Time.deltaTime;
     }
 }
